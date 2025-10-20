33c1b4192d41903bcd62830df18d6c37ef92c538
josedavidteixeira <jose.david@aluno.ifsertao-pe.edu.br>
2025-10-20 12:43:17 -0300

    Alterações até a aula 11

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 70.0
+const SPEED = 90.0
 const JUMP_VELOCITY = -300.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += get_gravity().y * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
